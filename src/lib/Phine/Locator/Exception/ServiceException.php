fc65d67ccd894675035a00a4d1aa83b43e3390b1
Kevin Herrera <kevin@herrera.io>
2013-09-17 18:48:26 -0700

    Adding a service exception class.

---
+++ src/lib/Phine/Locator/Exception/ServiceException.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Phine\Locator\Exception;
+
+use Phine\Exception\Exception;
+
+/**
+ * Exception thrown for problems with a service.
+ *
+ * @author Kevin Herrera <kevin@herrera.io>
+ */
+class ServiceException extends Exception
+{
+}
